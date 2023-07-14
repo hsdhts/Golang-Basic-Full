71928c099310c8b200ea7248b8dec3373e5db648
husada20 <husadahutasoit@gmail.com>
2023-07-14 23:28:32 +0700

    if else

--- if-else.go
+++ if-else.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
+	name := "Husada"
+
+	if name == "Husada" {
+		fmt.Println("Hello Husada")
+	} else if name == "Joko" {
+		fmt.Println("Hello Joko")
+	} else if name == "Budi" {
+		fmt.Println("Hello Budi")
+	} else {
+		fmt.Println("Masukkan nama yang benar!")
+	}
+
+	//if with short statement
+	if length := len(name); length > 5 {
+		fmt.Println("Karakter nama terlalu panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
+	}
 }
