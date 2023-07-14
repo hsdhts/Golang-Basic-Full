cc7010ebe6c8a42490e92cc1d02060ce923f0d93
husada20 <husadahutasoit@gmail.com>
2023-07-14 23:25:20 +0700

    map

--- map.go
+++ map.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	person := map[string]string{
+		"name":    "Husada",
+		"address": "Palembang",
+	}
+	person["title"] = "Software Engineer"
+
+	fmt.Println(person)
+	fmt.Println(person["name"])
+	fmt.Println(person["address"])
+	fmt.Println(person["title"])
+
+	book := make(map[string]string)
+	book["title"] = "Belajar Go-lang"
+	book["author"] = "Husada"
+	book["wrong"] = "Ups salah!"
+	fmt.Println(book)
+	fmt.Println(len(book))
+
+	delete(book, "wrong")
+	fmt.Println(book)
+	fmt.Println(len(book))
 
 }
