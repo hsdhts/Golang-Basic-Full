f70361104dfdb8a4b58c7bb0f362747cf6e23e60
husada20 <husadahutasoit@gmail.com>
2023-07-14 23:25:53 +0700

    string

--- string.go
+++ string.go
@@ -6,7 +6,9 @@ func main() {
 	fmt.Println("Husada")
 	fmt.Println("Hutasoit")
 	fmt.Println("Husada Hutasoit")
+	fmt.Println("something")
 
+	fmt.Println(len("something"))
 	fmt.Println(len("Husada"))
 	fmt.Println("Hutasoit"[0])
 	fmt.Println("Husada Hutasoit"[1])
