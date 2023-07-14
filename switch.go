b281fb699944cacf9502e14dd87b1e7be3ec1060
husada20 <husadahutasoit@gmail.com>
2023-07-14 23:26:28 +0700

    switch

--- switch.go
+++ switch.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	name := "Budi"
+
+	switch name {
+	case "Husada":
+		fmt.Println("Hello Husada")
+	case "Budi":
+		fmt.Print("Hello Budi")
+	default:
+		fmt.Print("Masukkan nama yang benar")
+	}
+
+	//switch with short statement
+	switch length := len(name); length > 5 {
+	case true:
+		fmt.Println("Karakter nama terlalu panjang")
+	case false:
+		fmt.Println("Nama sudah sesuai")
+	}
 
 }
