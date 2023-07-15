c85b9b76579680526edbce97a46a83224b7b0e7e
husada20 <husadahutasoit@gmail.com>
2023-07-15 16:51:33 +0700

    function return value named

--- function-return-value-name.go
+++ function-return-value-name.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func getFullName2() (firstName string, middleName string, lastName string) {
+	firstName = "Husada"
+	middleName = "Karunia"
+	lastName = "Hutasoit"
+
+	return
+}
+
+func main() {
+	firstName, middleName, lastName := getFullName2()
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
 }
