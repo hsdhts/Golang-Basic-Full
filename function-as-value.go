67c0ccca322ad4c97c97d9d55842f47ce0ba8532
husada20 <husadahutasoit@gmail.com>
2023-07-15 22:33:02 +0700

    func as value

--- function-as-value.go
+++ function-as-value.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func getGoodBye(name string) string {
+	return "Good Bye" + name
+}
+
+func main() {
+	sayGoodBye := getGoodBye
+	fmt.Println(sayGoodBye(" Husada"))
 }
