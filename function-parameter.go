73a1b395fc49f51a0cac4f45445af1d4edf02b56
husada20 <husadahutasoit@gmail.com>
2023-07-15 16:52:15 +0700

    function parameter

--- function-parameter.go
+++ function-parameter.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func sayHelloto(firstName string, lastName string, umur int) {
+	fmt.Println("Hello", firstName, lastName, umur)
+}
+
+func main() {
+	sayHelloto("Husada", "Hutasoit", 21)
 }
