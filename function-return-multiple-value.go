1bdf866ce5d411d38abc537d9d02b3ea7169486d
husada20 <husadahutasoit@gmail.com>
2023-07-15 16:49:25 +0700

    function return multiple value

--- function-return-multiple-value.go
+++ function-return-multiple-value.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func getFullName() (string, string, string) {
+	return "Husada", "Karunia", "Hutasoit"
+}
+
+func main() {
+	firstName, _, lastName := getFullName()
+	fmt.Println(firstName)
+	//fmt.Println(middleName) contoh: untuk ignore value yang tidak digunakan
+	fmt.Println(lastName)
 }
