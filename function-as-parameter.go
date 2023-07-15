9d720572fd98a4d74c12fb72a82db6310386494a
husada20 <husadahutasoit@gmail.com>
2023-07-15 22:32:23 +0700

    func as parameter

--- function-as-parameter.go
+++ function-as-parameter.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type Filter func(string) string
+
+func sayHelloWithFilter(name string, filter Filter) {
+	nameFiltered := filter(name)
+	fmt.Println(nameFiltered)
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing" {
+		return "****"
+	} else {
+		return name
+	}
+}
+
+func main() {
+	sayHelloWithFilter("Husada", spamFilter)
+	sayHelloWithFilter("Anjing", spamFilter)
 }
