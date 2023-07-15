7dbfb90fa5aeec7837aaa89f46b8e8dfb28e688a
husada20 <husadahutasoit@gmail.com>
2023-07-15 22:35:53 +0700

    func variadic

--- function-variadic.go
+++ function-variadic.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func sumAll(numbers ...int) int {
+	total := 0
+
+	for _, value := range numbers {
+		total += value
+	}
+	return total
+}
+
+func main() {
+	total := sumAll(10, 20, 30, 40)
+	fmt.Println(total)
+
+	slice := []int{10, 30, 40, 60}
+	total = sumAll(slice...)
+	fmt.Println(total)
 }
