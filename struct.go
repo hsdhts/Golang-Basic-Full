101e508dbb50556630e49c54dcf10d07df601dd4
husada20 <husadahutasoit@gmail.com>
2023-07-17 01:06:58 +0700

    struct

--- struct.go
+++ struct.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Data struct {
+	Name, Address string
+	Age           int
+}
+
+func (customer Data) sayHi(name string) {
+	fmt.Println("Hello", name, "My name is", customer.Name)
+}
+
+func (a Data) sayHuu() {
+	fmt.Println("Huuu from", a.Name)
+}
+
 func main() {
+
+	var husada Data
+	husada.Name = " Husada"
+	husada.Address = "Palembang"
+	husada.Age = 21
+
+	husada.sayHi("Joko")
+	husada.sayHuu()
+	//sayHi(husada, "Joko")
+
+	//var husada Data
+	//
+	//husada.Name = "Husada"
+	//husada.Address = "Palembang"
+	//husada.Age = 21
+	//
+	//fmt.Println(husada)
+	//
+	//joko := Data{
+	//	Name:    "Joko",
+	//	Address: "Cirebon",
+	//	Age:     20,
+	//}
+	//
+	//fmt.Println(joko)
+	//
+	//budi := Data{"Budi", "Medan", 19}
+	//
+	//fmt.Println(budi)
 
 }
