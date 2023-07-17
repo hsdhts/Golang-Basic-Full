081fb612214ded6c9d85148a024423c3f8b1ce0b
husada20 <husadahutasoit@gmail.com>
2023-07-18 03:04:59 +0700

    interface

--- interface.go
+++ interface.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(name HasName) {
+	fmt.Println("Hello", name.GetName())
+}
+
+type Test struct {
+	Name string
+}
+
+func (person Test) GetName() string {
+	return person.Name
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	var husada Test
+	husada.Name = "Husada"
+	SayHello(husada)
+
+	cat := Animal{
+		Name: "Push",
+	}
+	SayHello(cat)
 }
