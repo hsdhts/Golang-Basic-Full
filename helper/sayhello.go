9758e3dbc0dc38b41ea1663606d212790de7cfb6
husada20 <husadahutasoit@gmail.com>
2023-07-21 18:51:01 +0700

    golang dasar

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,12 @@ package helper
 
 import "fmt"
 
+var Application = "Backend Golang"
+
 func SayHello(name string) {
 	fmt.Println("Hello", name)
+}
+
+func sayGoodBye(age int) {
+	fmt.Println("Umur saya", age)
 }
