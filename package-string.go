61b9ca348dbedef7e3a4b46ec1d72c4ed7e96b59
husada20 <husadahutasoit@gmail.com>
2023-07-21 23:56:51 +0700

    golang dasar

--- package-string.go
+++ package-string.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	fmt.Println(strings.Contains("Black Mamba", "Black"))
+	fmt.Println(strings.Split("Husada Hutasoit", ""))
+	fmt.Println(strings.ToLower("Husada Hutasoit"))
+	fmt.Println(strings.ToUpper("husada hutasoit"))
+	fmt.Println(strings.Trim("    Husada Hutasoit    ", " "))
+	fmt.Println(strings.ReplaceAll("Husada Husada Husada Husada", "Husada", "Budi"))
 }
