993357dd512bccb0f50ab766cf227226b58ff280
husada20 <husadahutasoit@gmail.com>
2023-07-21 23:57:20 +0700

    golang dasar

--- container-list.go
+++ container-list.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+)
+
 func main() {
 
+	data := list.New()
+
+	data.PushBack("Husada")
+	data.PushBack("Karunia")
+	data.PushBack("Hutasoit")
+	data.PushFront("Budi")
+
+	for element := data.Back(); element != nil; element.Prev() {
+		fmt.Println(element.Value)
+	}
 }
