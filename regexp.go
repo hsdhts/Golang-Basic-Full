b0ad74ba84296b8e6ce53609c69439826d05b2e5
husada20 <husadahutasoit@gmail.com>
2023-07-22 22:14:50 +0700

    golang dasar

--- regexp.go
+++ regexp.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	regexp2 "regexp"
+)
+
 func main() {
 
+	var regexp = regexp2.MustCompile("e([a-z])o")
+
+	fmt.Println(regexp.MatchString("eko"))
+	fmt.Println(regexp.MatchString("budi"))
+	fmt.Println(regexp.MatchString("hasan"))
+
+	search := regexp.FindAllString("eko, edo, eto, eyo, eki", -1)
+
+	fmt.Println(search)
 }
