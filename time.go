e5cb3b33d8e8aae2387951bc198726983e684d2e
husada20 <husadahutasoit@gmail.com>
2023-07-22 22:14:27 +0700

    golang dasar

--- time.go
+++ time.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+
+	now := time.Now()
+
+	fmt.Println(now)
+	fmt.Println(now.YearDay())
+	fmt.Println(now.Month())
+	fmt.Println(now.Day())
+
+	utc := time.Date(2020, 10, 10, 10, 10, 10, 10, time.UTC)
+
+	fmt.Println(utc)
+
+	layout := "2020-01-24"
+
+	parse, _ := time.Parse(layout, "2020-01-23")
+	fmt.Println(parse)
 
 }
